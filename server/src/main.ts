3fbe90f89b8f60bcbfb37ef18131d58553fefd8c
Marty Kane <marty.kane@gmail.com>
2024-06-16 15:32:17 -0500

    Add global validation pipe to enforce properties on DTOs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 3000;
+const devMode = true;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true, // strip properties with no validation decorators in DTO
+      disableErrorMessages: false,
+      enableDebugMessages: devMode,
+      validationError: {
+        value: false, // don't expose the values
+      },
+    }),
+  );
+
+  console.log(`Game Shelf Organizer app listening on ${port}`);
+  await app.listen(port);
 }
 bootstrap();
