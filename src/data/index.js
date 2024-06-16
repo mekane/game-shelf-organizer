45332f189e4423c88e5ea215ece5b42524331bee
Marty Kane <marty.kane@gmail.com>
2024-06-16 15:54:19 -0500

    remove unused modules

--- src/data/index.js
+++
@@ -1,3 +0,0 @@
-// import the bgg data util
-// provide code to import a BGG collection given a username
-// return cleaned-up data with properties initialized for the organizer app
--- src/history/index.js
+++
@@ -1,2 +0,0 @@
-// export save and load functions to persist and retrieve data from storage
-// wrap a LocalStorage module and provide a unique key for this app
