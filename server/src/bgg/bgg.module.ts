fbe2aa3202cb68473a6fbabff469b2544b307032
Marty Kane <marty.kane@gmail.com>
2025-05-04 23:41:22 -0500

    fix module imports

--- server/src/bgg/bgg.module.ts
+++ server/src/bgg/bgg.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CollectionService } from '@src/collection/collection.service';
 import { allCollectionEntities } from '@src/entities';
+import { GamesService } from '@src/games/games.service';
 import { BggService } from './bgg.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature(allCollectionEntities)],
   exports: [BggService],
-  providers: [BggService],
+  providers: [BggService, CollectionService, GamesService],
 })
 export class BggModule {}
